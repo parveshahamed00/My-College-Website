e82648e23b9bbaf1b9a6d3a890d3bb824afb0014
Parvesh <parveshahamed00@gmail.com>
2022-11-16 22:27:26 +0530

    creating the EJS file & sucessfully adding the functionality to see rrsult for students

--- controllers/result-post.js
+++ controllers/result-post.js
@@ -1,5 +1,26 @@
-exports.result=(req,res)=>{
-    // res.send("hello")
-    console.log(req.body);
+const mongoose = require("mongoose");
+const lodash = require("lodash");
+exports.result = (req, res) => {
+    const collection = mongoose.connection.db.collection("exam-marks");
+      collection.findOne(
+        { rollNo: lodash.toLower(req.body.rollNo) },
+        (err, docs) => {
+          if (!err) {
+            // console.log(docs);
+            if (docs === null) {
+              res.write("<h1>Error !!</h1>");
+              res.write("<h3>Student Not found in database</h3>");
 
-}
+              console.log("Student Not fount in database");
+            } else {
+              // console.log(docs);
+              res.render("result", { doc: docs });
+            }
+          } else {
+            console.log(err);
+            res.sendStatus(500);
+          }
+        }
+      );
+
+};

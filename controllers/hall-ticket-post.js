fda31a2ad003776bf3062432c255f11e89957f83
Parvesh <parveshahamed00@gmail.com>
2022-11-20 08:38:12 +0530

    changing the hall-ticket.html file to ejs done the POST route for hall-ticket, functionality of downloading the hall-ticket & implementing PAYTM button are remaining

--- controllers/hall-ticket-post.js
+++ controllers/hall-ticket-post.js
@@ -1,4 +1,26 @@
-exports.hallTicket=(req,res)=>{
-    res.send("Hooi")
-    console.log(req.body);
-}
+const mongoose = require("mongoose");
+const lodash = require("lodash");
+exports.hallTicket = (req, res) => {
+  const collection = mongoose.connection.db.collection("hall-tickets");
+
+  collection.findOne(
+    { rollNo: lodash.toLower(req.body.rollNo) },
+    (err, docs) => {
+      if (!err) {
+        // console.log(docs);
+        if (docs === null) {
+          res.write("<h1>Error !!</h1>");
+          res.write("<h3>Student Not found in database</h3>");
+
+          console.log("Student Not fount in database");
+        } else {
+          // console.log(docs);
+          res.render("hall-ticket", { doc: docs });
+        }
+      } else {
+        console.log(err);
+        res.sendStatus(500);
+      }
+    }
+  );
+};
